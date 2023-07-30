62e48450ff1dc2ffebb96d8039c6c681ab28fcf2
Fainner Ramirez <fainnerramirez@gmail.com>
2023-07-30 09:51:16 -0500

    update commit

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 import { LitElement, html } from "lit";
 
-@customElements('my-app')
 export class App extends LitElement {
+
+    static properties = {
+        name: {}
+    }
+
+    constructor(){
+        super()
+        this.name = "Fainner Ramirez"
+    }
+
+
     render(){
         return html`
-            <h1>Hola mundo</h1>
+            <h1>Hola ${this.name}</h1>
         `;
     }
 }
+
+customElements.define('my-app', App)
